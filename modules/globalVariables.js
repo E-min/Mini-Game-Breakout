204556b215ae55085f56f13db61ea80ffdd2c221
Mehmet Emin <eminkocabuga@gmail,com>
2023-05-10 01:03:34 +0300

    frame rate dropped

--- modules/globalVariables.js
+++ modules/globalVariables.js
@@ -1,5 +1,5 @@
 export const display = document.getElementById("display");
 export const context = display.getContext("2d");
-const fps = 60;
+const fps = 30;
 let gravity = 0;
 export { gravity, fps};
