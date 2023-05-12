d1fb006285d85492050d9509e012bbad3ee01fba
Mehmet Emin <eminkocabuga@gmail,com>
2023-05-12 17:45:59 +0300

    fps lowered

--- modules/globalVariables.js
+++ modules/globalVariables.js
@@ -2,7 +2,7 @@ export const display = document.getElementById("display");
 export const context = display.getContext("2d");
 export const infoScore = document.getElementById("score");
 export const miliseconds = Date.now();
-export const frameRate = 120;
+export const frameRate = 60;
 
 export function userHaveTouchScreen() {
   const buttonsContainer = document.getElementById("buttons-container");
